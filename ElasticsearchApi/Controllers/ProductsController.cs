37bc55c11badfe1a5a9654630b66c3bdcf65dd0f
rahmet-donmez <rhmtdnmz@gmail.com>
2024-09-19 17:26:54 +0300

    crud

---
+++ ElasticsearchApi/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using Elastic.Clients.Elasticsearch;
+using ElasticsearchAPI.Models;
+using ElasticsearchAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticsearchAPI.Controllers
+{
+    [Route("/api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly ElastichsearchService service;
+
+        public ProductsController(ElastichsearchService service)
+        {
+            this.service = service;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Save([FromBody] Product product)
+        {
+            await service.SaveProductAsync(product);
+            return Ok();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await service.GetAllProductAsync();
+            return Ok(products);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Product product)
+        {
+            await service.UpdateProductAsync(product);
+            return Ok();
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await service.DeleteProductAsync(id);
+            return Ok();
+        }
+
+
+    }
+}
---
+++ ElasticsearchApi/Services/ElastichsearchService.cs
@@ -0,0 +1,95 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.Core.Bulk;
+using ElasticsearchAPI.Models;
+using System;
+
+namespace ElasticsearchAPI.Services
+{
+    public class ElastichsearchService
+    {
+        private readonly ElasticsearchClient _elasticsearchClient;
+        public ElastichsearchService()
+        {
+            ElasticsearchClientSettings setting = new(new Uri("http://localhost:9200"));
+            setting.DefaultIndex("products");
+            ElasticsearchClient elasticsearchClient = new ElasticsearchClient(setting);
+            _elasticsearchClient = elasticsearchClient;
+
+        }
+
+      
+        public async Task<List<Product>> GetAllProductAsync()
+        {
+            SearchResponse<Product> searchResponse = await _elasticsearchClient.SearchAsync<Product>("products");
+            if (!searchResponse.IsValidResponse)
+            {
+                throw new Exception("Veri listeleme işleminde hata meydana geldi");
+            }
+
+            return searchResponse.Documents.ToList();
+
+        }
+        public async Task SaveProductAsync(Product product)
+        {
+            CreateRequest<Product> createRequest = new(product.Id.ToString())
+            {
+                Document = product
+            };
+            var createResponse = await _elasticsearchClient.CreateAsync(createRequest);
+            if (!createResponse.IsValidResponse)
+            {
+                throw new Exception("Veri ekleme işleminde hata meydana geldi");
+            }
+
+        }
+        public async Task UpdateProductAsync(Product product)
+        {
+
+            UpdateRequest<Product, Product> updateRequest = new("products", product.Id.ToString())
+            {
+                Doc = product
+            };
+            var updateResponse = await _elasticsearchClient.UpdateAsync(updateRequest);
+            if (!updateResponse.IsValidResponse)
+            {
+                throw new Exception("Veri güncelleme işleminde hata meydana geldi");
+            }
+
+
+        }
+        public async Task DeleteProductAsync(int id)
+        {
+
+            DeleteRequest deleteRequest = new("products", id.ToString());
+            CancellationToken cancellationToken = new CancellationToken();
+            var deleteResponse = await _elasticsearchClient.DeleteAsync(deleteRequest, cancellationToken);
+            if (!deleteResponse.IsValidResponse)
+            {
+                throw new Exception("Veri silme işleminde hata meydana geldi");
+
+            }
+
+        }
+
+        //Bulk, birden fazla işlemi (ekleme, güncelleme, silme)
+        //tek bir istekle gerçekleştirmeye olanak tanır
+        public async Task BulkAsync(int id)
+        {
+
+            var bulkRequest = new BulkRequest();
+
+            bulkRequest.Operations.Add(new BulkIndexOperation<Product>(new Product { Id = 1, Name = "Product 1", Price = 10 }));
+            bulkRequest.Operations.Add(new BulkIndexOperation<Product>(new Product { Id = 2, Name = "Product 2", Price = 20 }));
+            bulkRequest.Operations.Add(new BulkDeleteOperation<Product>("3"));  // ID'si 3 olan ürünü sil
+
+            var bulkResponse = await _elasticsearchClient.BulkAsync(bulkRequest);
+
+            if (!bulkResponse.IsValidResponse)
+            {
+                throw new Exception("Toplu işlem başarısız oldu.");
+            }
+
+
+        }
+    }
+}
